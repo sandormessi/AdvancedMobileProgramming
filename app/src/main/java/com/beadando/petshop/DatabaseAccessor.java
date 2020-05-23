19fbc973448b8d5fdaff7265213e539218f8c136
herenyiz2 <zoltan_herenyi@freemail.hu>
2020-05-23 17:24:35 +0200

    Improve database accessor

--- app/src/main/java/com/beadando/petshop/DatabaseAccessor.java
+++ app/src/main/java/com/beadando/petshop/DatabaseAccessor.java
@@ -1,84 +1,110 @@
 package com.beadando.petshop;
 
+import android.net.Uri;
+
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.Serializable;
 import java.util.HashMap;
 
 public final class DatabaseAccessor implements Serializable
 {
-    private static final DatabaseReference DATABASE_REFERENCE = FirebaseDatabase.getInstance().getReference();
+    private static final FirebaseDatabase FIREBASE_DATABASE = FirebaseDatabase.getInstance();
+    private static final StorageReference FIREBASE_STORAGE = FirebaseStorage.getInstance().getReference();
 
-    public static void getCollectionData(final DataCollectionProvider dataProvider, final String[] path)
+    public static void getCollectionDataAtPath(final DataCollectionProvider dataProvider, final String path, final boolean detachEventHandlerAfterFistOccurrence)
     {
-        DATABASE_REFERENCE.addValueEventListener(new ValueEventListener()
+        final DatabaseReference refAtPath = getReferenceAtPath(path);
+        final ValueEventListener eventListener = new ValueEventListener()
         {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot)
             {
-                DataSnapshot pathElement = dataSnapshot.child(path[0]);
+                dataProvider.ProvideCollection(dataSnapshot.getChildren());
 
-                for (int i = 1; i < path.length; i++)
+                if (detachEventHandlerAfterFistOccurrence)
                 {
-                    pathElement = pathElement.child(path[i]);
+                    refAtPath.removeEventListener(this);
                 }
-
-                dataProvider.ProvideCollection(pathElement.getChildren());
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) { }
-        });
+        };
+
+        refAtPath.addValueEventListener(eventListener);
     }
 
-    public static void getData(final DataProvider dataProvider, final String[] path)
+    public static void getDataAtPath(final DataProvider dataProvider, final String path, final boolean detachEventHandlerAfterFistOccurrence)
     {
-        DATABASE_REFERENCE.addListenerForSingleValueEvent(new ValueEventListener()
+        final DatabaseReference refAtPath = getReferenceAtPath(path);
+        final ValueEventListener eventListener = new ValueEventListener()
         {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot)
             {
-                DataSnapshot pathElement = dataSnapshot.child(path[0]);
+                dataProvider.ProvideData(dataSnapshot);
 
-                for (int i = 1; i < path.length; i++)
+                if(detachEventHandlerAfterFistOccurrence)
                 {
-                    pathElement = pathElement.child(path[i]);
+                    refAtPath.removeEventListener(this);
                 }
-
-                dataProvider.ProvideData(pathElement);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) { }
-        });
+        };
+
+        refAtPath.addValueEventListener(eventListener);
     }
 
-    public static void addDataWithListener(@NonNull final OnCompleteListener listener, final HashMap<String, Object> data, final String[] path)
+    public static void addOrUpdateDataWithListener(@NonNull final OnCompleteListener listener, final Object data, final String path)
     {
-        DATABASE_REFERENCE.addListenerForSingleValueEvent(new ValueEventListener()
-        {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
-            {
-               DatabaseReference child = DATABASE_REFERENCE.child(path[0]);
+        final DatabaseReference refAtPath = getReferenceAtPath(path);
+        refAtPath.setValue(data).addOnCompleteListener(listener);
+    }
 
-                for (int i = 1; i < path.length; i++)
+    public static void addOrUpdateCollectionDataWithListener(@NonNull final OnCompleteListener listener, final HashMap<String, Object> data, final String path)
+    {
+        final DatabaseReference refAtPath = getReferenceAtPath(path);
+        refAtPath.updateChildren(data).addOnCompleteListener(listener);
+    }
+
+    public static void addOrUpdateFileWithListener(@NonNull final OnCompleteListener listener, final Uri filePath, final String storageName)
+    {
+        final StorageReference refAtPath = FIREBASE_STORAGE.child(storageName);
+        refAtPath.putFile(filePath).addOnCompleteListener(listener);
+    }
+
+    public static void getFileAtPath(final BinaryFileProvider binaryFileProvider, final String fileDbPath)
+    {
+        StorageReference pathReference =
+                FIREBASE_STORAGE
+                .child("Product Images")
+                .child(fileDbPath);
+        pathReference
+            .getBytes(2 * 1024 * 1024)
+            .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes)
                 {
-                    child = child.child(path[i]);
+                    binaryFileProvider.ProvideFile(bytes);
                 }
+            });
+    }
 
-                child.updateChildren(data).addOnCompleteListener(listener);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) { }
-        });
+    private static DatabaseReference getReferenceAtPath(final String path)
+    {
+        return FIREBASE_DATABASE.getReference(path);
     }
 }
